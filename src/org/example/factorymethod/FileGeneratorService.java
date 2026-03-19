7fba7e9ff80ac871f82afebb5eaca75a1c4ea6fe
Aditi Rai <aditi_rai@epam.com>
2026-03-19 16:13:15 +0530

    i have created factory method

--- src/org/example/factorymethod/FileGeneratorService.java
+++ src/org/example/factorymethod/FileGeneratorService.java
@@ -1,6 +1,5 @@
 package org.example.factorymethod;
 //Client class that uses the factory to generate files.
-
 public class FileGeneratorService {
     public static void main(String[] args){
         // Create factory object
